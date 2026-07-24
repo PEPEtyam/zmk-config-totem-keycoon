f8c47cf7560b74bd77ed15918d62f710ad0764a8
PEPEtyam <vlad.shkinderov@gmail.com>
2026-07-24 16:38:47 +0300

    fix(keymap): route universal symbols through kanata

--- zmk-ruen-compat/src/ruen_endpoints_compat.c
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-#include <zmk/endpoints.h>
-
-/* zmk-ruen still uses the pre-2023 endpoints API name. */
-int zmk_endpoints_send_report(uint16_t usage_page) {
-    return zmk_endpoint_send_report(usage_page);
-}
